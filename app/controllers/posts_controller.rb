544b3d716efd00d0aceee0d13dc1f63a342094c0
rmzNadir <dgzrz99@gmail.com>
2021-03-22 17:21:50 -0600

    create / update posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  # Responds with status code 500 on every other error
+  rescue_from Exception do |e|
+    render json: { error: e.message }, status: :internal_error
+  end
+
+  # Responds with status code 422 on invalid record
+  rescue_from ActiveRecord::RecordInvalid do |e|
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
+
   # GET /posts
   def index
     @posts = Post.where(published: true)
@@ -12,5 +22,30 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     render json: @post, status: :ok
+  end
+
+  # POST /posts
+  def create
+    @post = Post.create!(create_params)
+    render json: @post, status: :created
+  end
+
+  # PUT /posts/:id
+  def update
+    @post = Post.find(params[:id])
+
+    @post.update!(update_params)
+
+    render json: @post, status: :ok
+  end
+
+  private
+
+  def create_params
+    params.require(:post).permit(:title, :content, :published, :user_id)
+  end
+
+  def update_params
+    params.require(:post).permit(:title, :content, :published)
   end
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -39,7 +39,88 @@ RSpec.describe 'Posts', type: :request do
       payload = JSON.parse(response.body)
       expect(payload).to_not be_empty
       expect(payload['id']).to eq(post.id)
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
+  end
+
+  describe 'POST /posts' do
+    let!(:user) do
+      create(:user)
+    end
+    it 'should create a post' do
+      req_payload = {
+        post: {
+          title: 'título',
+          content: 'content',
+          published: false,
+          user_id: user.id
+        }
+      }
+
+      post '/posts', params: req_payload
+
+      payload = JSON.parse(response.body)
+      expect(payload).to_not be_empty
+      expect(payload['id']).to_not be_nil
+      expect(response).to have_http_status(:created)
+    end
+
+    it 'should return error msg on invalid post' do
+      req_payload = {
+        post: {
+          title: '',
+          content: 'content',
+          published: false,
+          user_id: user.id
+
+        }
+      }
+
+      post '/posts', params: req_payload
+
+      payload = JSON.parse(response.body)
+      expect(payload).to_not be_empty
+      expect(payload['error']).to_not be_empty
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+end
+
+describe 'PUT /posts/:id' do
+  let!(:article) do
+    create(:post)
+  end
+  it 'should update a post' do
+    req_payload = {
+      post: {
+        title: 'título nuevo',
+        content: 'content nuevo',
+        published: true
+      }
+    }
+
+    put "/posts/#{article.id}", params: req_payload
+
+    payload = JSON.parse(response.body)
+    expect(payload).to_not be_empty
+    expect(payload['id']).to eq(article.id)
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'should return error msg on invalid post' do
+    req_payload = {
+      post: {
+        title: nil,
+        content: nil,
+        published: true
+      }
+    }
+
+    put "/posts/#{article.id}", params: req_payload
+
+    payload = JSON.parse(response.body)
+    expect(payload).to_not be_empty
+    expect(payload['error']).to_not be_empty
+    expect(response).to have_http_status(:unprocessable_entity)
   end
 end
